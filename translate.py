e7e42948194f03fed76139f627d69fe1e348b000
DazedFury <jmtexas19@gmail.com>
2021-06-06 12:20:26 -0500

    Create only one instance for each thread

--- translate.py
+++ translate.py
@@ -8,9 +8,19 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from lxml import html
 
+#Number of threads to create
+THREADS = 10
+translationObjList = [None] * THREADS
+
+#Selenium
+options = Options()
+options.headless = True
+options.add_argument("--disable-web-security")
+options.add_argument('log-level=2')
+
 #Regex
-pattern1 = re.compile(r'([一-龠ぁ-ゔァ-ヴーａ-ｚＡ-Ｚ０-９々〆〤～！？＋、<>…･・。♥　”゛【】%0-9A-Za-z\\\[\] ]+)') #Main Matching Regex
-pattern2 = re.compile(r'([一-龠ぁ-ゔァ-ヴー々〆〤]+)') #Filter Matches with no Japanese Text
+pattern1 = re.compile(r'([一-龠ぁ-ゔァ-ヴーａ-ｚＡ-Ｚ０-９々〆〤～！？＋、<>…_･・。♥★　”゛【】%0-9A-Za-z\\\[\] ]+)') #Main Matching Regex
+pattern2 = re.compile(r'([一-龠ぁ-ゔァ-ヴー々〆〤～]+)') #Filter Matches with no Japanese Text
 pattern3 = re.compile(r'([\\]+[a-zA-Z0-9]+\[[0-9]+\]|[\\]+[a-zA-Z]+<[\\]+[a-zA-Z]+\[[0-9]+\]>|[\\]+[a-zA-Z]+<[-a-zA-Z]+.[\\]+.|[\\]+>)') #Filter for variables (e.g \\n[2])
 
 #Create Directory
@@ -21,42 +31,71 @@ choice = 0
 while not(choice == '1' or choice == '2' or choice == '3'):
     choice = input('Choose Translation Type:\n[1] Translate only matches\n[2] Translate entire line\n')
 
-class textObject:
+#Class to hold translation data
+class translationObj:
     variableList = []
     text = ''
+    lock = 0
+    driver = None
+
+    def __init__(self):
+        driver = webdriver.Chrome('chromedriver.exe', options=options)
+        driver.set_page_load_timeout(15)
+        driver.implicitly_wait(15) 
+        self.driver = driver  
+
+    #Assign driver if not locked
+    def getDriver(self):
+        if (self.lock == 0):
+            self.lock = 1
+            return self.driver
+        else:
+            return None
+    
+    def release(self):
+        self.lock = 0
+
+#Create drivers for translation based on THREADS
+def createDrivers():
+    for i in range(len(translationObjList)):
+        translationObjList[i] = translationObj()
 
 #Translate Via DeepL
 def translate(text):
-    #Selenium
-    options = Options()
-    options.headless = True
-    driver = webdriver.Chrome('chromedriver.exe', options=options)
+    try:         
+        #Assign object to thread
+        tO = None
+        driver = None
+        while(tO == None):
+            for obj in translationObjList:
+                if(obj.lock == 0):
+                    tO = obj
+                    tO.text = text
+                    driver = tO.getDriver()
+                    break   
 
-    #Assign to new object
-    tO = textObject()
-    tO.text = text
+        #Get Page and Translate
+        print('Translating: ' + tO.text + ' using driver ' + str(driver))
+        tO = filterVariables(tO)
+        url = 'https://www.deepl.com/translator#ja/en/' + tO.text
+        driver.get(url)
 
-    #Get Page and Translate
-    tO = filterVariables(tO)
-    url = 'https://www.deepl.com/translator#ja/en/' + tO.text
-    driver.get(url)
-
-    #Wait until translation is finished loading
-    try:
+        #Wait until translation is finished loading
         match = WebDriverWait(driver, 10).until(lambda driver: 
             re.search(r'^(?!\s*$).+', driver.find_element_by_id('target-dummydiv').get_attribute("innerHTML"))
         )
+    
+        #Clean
+        tO.text = match.group()
+        tO.text = tO.text.strip()
+        tO = filterVariables(tO)
+        tO.release()
+        return tO.text
+        
     except TimeoutException:
-        print('Failed to find translation')
-        time.sleep(10)
-        driver.close()
-        filterVariables(tO)
-
-    tO.text = match.group()
-    tO.text = tO.text.strip()
-    tO = filterVariables(tO)
-    driver.close()
-    return tO.text
+        print('Failed to find translation for line: ' + tO.text)
+        tO.release()
+        return tO.text
 
 #Filter variables from string, then put back
 def filterVariables(tO):
@@ -104,26 +143,27 @@ def filterVariables(tO):
     return tO
 
 def main():
+    createDrivers()
+    print(translationObjList)
+
     if(choice == '3'):
         translate('実際、この日まで\\n[10]は\"人間の男\"とはセックスどころか、')
         quit()
         
-
     # Open File
     for filename in os.listdir("files"):
         with open('translate/' + filename, 'w', encoding='UTF-8') as outFile:
             with open('files/' + filename, 'r', encoding='UTF-8') as f:
 
                 # Replace Each Line
-                with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+                with concurrent.futures.ThreadPoolExecutor(max_workers=THREADS) as executor:
 
-                    # The following submit all lines
+                    # The following submits all lines
                     fs = [executor.submit(findMatch, line) for line in f]
 
                     # as_completed return arbitrary future when it is done
                     # Use simple for-loop ensure the future are iterated sequentially
                     for future in fs:
-                        # Uncomment to actually write to the output
                         outFile.write(future.result())
 
 
@@ -135,14 +175,13 @@ def findMatch(line):
         for match in re.findall(pattern1, line):
 
             # Filter out matches with no Japanese
-            if (re.search(pattern2, match)):
+            if (re.search(pattern2, match) and "Call" not in match and "Para" not in match and '' != match):
                 if (choice == '1'):
-                    print('Translating: ' + match + '\n')
                     translatedMatch = translate(match)
-                    line = line.replace(match, translatedMatch, 1)
+                    line = re.sub(r'(?<!\w)' + re.escape(match) + r'(?!\w)', translatedMatch, line, 1)
 
                 elif (choice == '2'):
-                    line = translate(line)
+                    line = translate(match)
                     break  # Don't want dupes
 
                 else:
@@ -152,7 +191,6 @@ def findMatch(line):
     # Skip Line
     else:
         print('Skipping: ' + line)
-
         return line
 
 start = time.time()
